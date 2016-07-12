a0ae5d498bb5c3eb898bbc6e1d862c5f786fc502
Charlie Brookhouse <cbrookhouse8@gmail.com>
2016-07-12 14:58:26 -0400

    Switch to d3 v4. Implement cross-hairs.

--- drag/main.js
+++ drag/main.js
@@ -1,11 +1,6 @@
 // Nb. at the moment, I don't like the non-uniform scaling of this chart
-       var dataset = [
-                       [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-                       [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
-                     ];       
-       
-           dataset = [];
-            
+       var dataset = [];
+
            for (var i = 0 ; i < 2; i++) {
                 dataset.push([Math.random()*1200,Math.random()*1100]);
            }
@@ -13,18 +8,15 @@
        var h = 400;
        var w = 600; 
        var pad = 30;
-       var startDrag = false;
-       var dragging = true;
-       var endDrag = true;
-       var delta = [0,0];
        var setLines;
+       
        // Scales
 
-       var x_scale = d3.scale.linear().domain([0,d3.max(dataset,d => d[0])])
-                                      .range([pad,w-2*pad]);
+       var x_scale = d3.scaleLinear().domain([0,d3.max(dataset,d => d[0])])
+                                     .range([pad,w-2*pad]);
 
-       var y_scale = d3.scale.linear().domain([0,d3.max(dataset,d => d[1])])
-                                      .range([h-pad,pad]); 
+       var y_scale = d3.scaleLinear().domain([0,d3.max(dataset,d => d[1])])
+                                     .range([h-pad,pad]); 
 /*
        var r_scale = d3.scale.linear().domain([0,d3.max(dataset,d => d[1])])
                                       .range([2,5]);
@@ -33,55 +25,59 @@
        // Chart and data
        var bg = d3.rgb(196,228,255);
        var svg = d3.select("body").append("svg")
+                                  .attr("id", "chart")
                                   .attr("width",w)
                                   .attr("height",h)
                                   .style("background-color",bg);
 
-        svg.selectAll("line").data(dataset)
-                             .enter()
-                             .append("line")
-                             .style("stroke","black");
+           svg.append("line").attr("id","touchY");
+           svg.append("line").attr("id","touchX");
+             
+        svg.selectAll("line").style("stroke","black").attr("class","axis_readoff");
 
-        setLines = () => {
-               svg.selectAll("line").data(dataset)
-                                    .attr("x1",0)
-                                    .attr("y1",d => y_scale(d[1]))
-                                    .attr("x2",d => x_scale(d[0]))
-                                    .attr("y2",d => y_scale(d[1]));
-        }      
+        setLines = (px,py) => {
+               svg.select("#touchY").attr("x1",pad)
+                                    .attr("y1",py)
+                                    .attr("x2",px)
+                                    .attr("y2",py);
 
-        setLines();
+               svg.select("#touchX").attr("x1",px)
+                                    .attr("y1",py)
+                                    .attr("x2",px)
+                                    .attr("y2",h-pad);
+                              }      
 
-          var drag = d3.behavior.drag()
+          var drag = d3.drag()
                 .on("drag", function(d,i) {
-
-                var unitX = x_scale(1) - x_scale(0);
-                var unitY = y_scale(1) - y_scale(0);
-                
-                d[0] += d3.event.dx / unitX;
-                d[1] += d3.event.dy / unitY;
-/*
-                    d3.select(this).attr("transform", function(d,i){
-                        return "translate(" + [ x_scale(d[0]),y_scale(d[1]) ] + ")";
-                    });
-*/
-                    d3.select(this)
-                      .attr("cx",x_scale(d[0]))
-                      .attr("cy",y_scale(d[1]));
+                    var unitX = x_scale(1) - x_scale(0);
+                    var unitY = y_scale(1) - y_scale(0);
                     
-                    setLines();
-                }); 
+                    d[0] += d3.event.dx / unitX;
+                    d[1] += d3.event.dy / unitY;
+                    
+                    var px = x_scale(d[0]);
+                    var py = y_scale(d[1]);
 
-           svg.selectAll("circle").data(dataset)
+                        d3.select(this)
+                          .attr("cx",px)
+                          .attr("cy",py);
+                        
+                        setLines(px,py);
+                }); 
+          
+          var circles = svg.selectAll("circle").data(dataset)
                                   .enter()
                                   .append("circle")
                                   .attr("cx",d => x_scale(d[0]))
                                   .attr("cy",d => y_scale(d[1]))
                                   //.attr("r",d => r_scale(d[1]))
-                                  .attr("r",10)
+                                  .attr("r",6)
                                   .call(drag);
-
-           
+             /* 
+              circles.on('click', function(data,index){
+                                        console.log(data);
+                                  });
+             */
 
                svg.selectAll("text").data(dataset)
                                 .enter()
@@ -92,12 +88,13 @@
                                 .attr("font-family", "sans-serif")
                                 .attr("font-size", "11px")
                                 .attr("fill", "steelblue");
+
         // Axes
         
         // see also the tickFormat(), which is mentioned in the tutorial
         
         // this is version 4 syntax
-/*
+
         var xAxis = d3.axisBottom(x_scale).ticks(5);
         var yAxis = d3.axisLeft(y_scale).ticks(5);
 
@@ -110,4 +107,4 @@
                .attr("class","axis")    // to help with styling 
                .attr("transform","translate("+pad+",0)") // shift to right 
                .call(yAxis);
-*/
+
